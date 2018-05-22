2c6f0f4b0aed791006631da9eaec068d20a9608f
aleksipousar <aleksi1578@msn.com>
2018-05-22 19:20:26 +0300

    it was still trying to access region

--- android/src/main/java/com/mackentoch/beaconsandroid/BeaconsAndroidModule.java
+++ android/src/main/java/com/mackentoch/beaconsandroid/BeaconsAndroidModule.java
@@ -309,8 +309,8 @@ public class BeaconsAndroidModule extends ReactContextBaseJavaModule implements 
             WritableMap b = new WritableNativeMap();
             b.putString("uuid", beacon.getId1().toString());
             if (beacon.getIdentifiers().size() > 2) {
-                b.putInt("major", beacon.getId2() != null ? region.getId2().toInt() : 0);
-                b.putInt("minor", beacon.getId3() != null ? region.getId3().toInt() : 0);
+                b.putInt("major", beacon.getId2() != null ? beacon.getId2().toInt() : 0);
+                b.putInt("minor", beacon.getId3() != null ? beacon.getId3().toInt() : 0);
             }
             b.putInt("rssi", beacon.getRssi());
             b.putDouble("distance", beacon.getDistance());
